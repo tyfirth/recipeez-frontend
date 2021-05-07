77db55baeb6718e53259325bc35a8da4649e945a
tyfirth <tyfirth@gmail.com>
2021-05-06 17:31:28 -0700

    removing uneeded components. starting as fresh as possible to make it more manageable..for me

--- src/containers/RecipeContainer.js
+++ src/containers/RecipeContainer.js
@@ -15,44 +15,10 @@ state = {
   recipes: []
 }
 
-  componentDidMount() {
-    if (this.state.recipes.length === 0) {
-      this.props.getRandomRecipes()
-    } else {
-        this.props.handleSearch()
-    }
-  }
-
-
-  render() {
-    return (
-      <div class='bg-blue-400'>
-        <div>
-        <RecipeForm handleSearch={this.handleSearch} /><br/>
-        </div>
-        <RecipesList recipes={this.props.recipes} />
-      </div>
-    )
-  }
-
-
-  handleSearch = (query) => {
-    fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${query}&instructionsRequired=true&addRecipeInformation=true&fillIngredients=true&number=3&apiKey=${apiKey}`)
-      .then(resp => resp.json())
-      .then(data => {
-        this.setState({
-          recipes: data.results
-        })
-      })
-  }
 
 }
 
-const mapStateToProps = state => {
-  return {
-      recipes: state.recipes
-  }
-}
 
 
-export default connect(mapStateToProps, {fetchRecipes, getRandomRecipes, handleSearch})(RecipeContainer);
+
+export default RecipeContainer;

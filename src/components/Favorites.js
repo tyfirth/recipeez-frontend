995e5dfc5c188af9a4c7734d9d95738dc7d238e5
tyfirth <tyfirth@gmail.com>
2021-05-12 22:11:38 -0700

    things are mostly working. as usual before assessment

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,10 +1,16 @@
 import React, {Component} from 'react';
 
+
 class Favorites extends Component {
+
+  componentDidMount() {
+    console.log(this.props)
+  }
+
   render() {
     return (
       <div className='favorites' class='hover:text-white'>
-        This is the favorites section.
+        
       </div>
     )
   }
--- src/containers/MenuContainer.js
+++ src/containers/MenuContainer.js
@@ -1,19 +1,30 @@
 import React, {Component} from 'react';
 import About from '../components/About'
 import Favorites from '../components/Favorites'
-import {Route} from 'react-router-dom'
+import {Route, Link} from 'react-router-dom'
+import {connect} from 'react-redux'
 
 class MenuContainer extends Component {
 
   render() {
     return(
       <div className='menu-container' class='bg-blue-400 p-4 flex justify-around text-lg font-bold'>
+
+        <Link to='/about'>About</Link>
         <Route exact path='/about' component={About} />
+
+        <Link to='/favorites'>Favorites</Link>
         <Route exact path='/favorites'
-          render={() => <Favorites /> } />
+          render={() => <Favorites recipes={this.props.recipes}/> } />
       </div>
     )
   }
 }
 
-export default MenuContainer;
+const mapStateToProps = state => {
+  return {
+    recipes: state.recipes
+  }
+}
+
+export default connect(mapStateToProps)(MenuContainer);
